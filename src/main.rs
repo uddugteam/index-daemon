866db5a980372eb1e342362f214a8ca36d568d83
Alexander Aglerimov <formiat95@gmail.com>
2021-12-12 23:20:38 +0300

    done: change app configuration flow - applied crate "config"

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,53 @@ use clap::App;
 
 mod worker;
 
-fn main() {
+fn get_config_file_path(key: &str) -> Option<String> {
     let yaml = load_yaml!("../resources/cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let markets: Option<Vec<&str>> = matches.values_of("market").map(|v| v.into_iter().collect());
-    let coins: Option<Vec<&str>> = matches.values_of("coin").map(|v| v.into_iter().collect());
+    matches.value_of(key).map(|v| v.to_string())
+}
+
+fn get_config(key: &str) -> config::Config {
+    let mut market_config = config::Config::default();
+    market_config
+        .merge(config::File::with_name("./resources/market_config_default"))
+        .unwrap();
+
+    if let Some(path) = get_config_file_path(key) {
+        market_config.merge(config::File::with_name(&path)).unwrap();
+    } else {
+        market_config
+            .merge(config::Environment::with_prefix("APP_MARKET").separator("_"))
+            .unwrap();
+    }
+
+    market_config
+}
+
+fn get_param_value_as_vec_of_string(config: &config::Config, key: &str) -> Option<Vec<String>> {
+    if let Ok(string) = config.get_str(key) {
+        Some(string.split(',').map(|v| v.to_string()).collect())
+    } else {
+        config
+            .get_array(key)
+            .ok()
+            .map(|v| v.into_iter().map(|v| v.into_str().unwrap()).collect())
+    }
+}
+
+fn main() {
+    let market_config = get_config("market_config");
+
+    let markets: Option<Vec<String>> = get_param_value_as_vec_of_string(&market_config, "markets");
+    let markets: Option<Vec<&str>> = markets
+        .as_ref()
+        .map(|v| v.iter().map(|v| v.as_str()).collect());
+
+    let coins: Option<Vec<String>> = get_param_value_as_vec_of_string(&market_config, "coins");
+    let coins: Option<Vec<&str>> = coins
+        .as_ref()
+        .map(|v| v.iter().map(|v| v.as_str()).collect());
 
     let (tx, rx) = mpsc::channel();
     let worker = Worker::new(tx);
