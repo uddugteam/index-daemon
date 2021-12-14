3b16a6f521de5618cc399541aed831e9b8d1cbd4
Alexander Aglerimov <formiat95@gmail.com>
2021-12-14 20:25:40 +0300

    feat: removed panic on connect from "SocketHelper::run()"

--- src/worker/network_helpers/socket_helper.rs
+++ src/worker/network_helpers/socket_helper.rs
@@ -43,33 +43,34 @@ where
 {
     let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
 
-    // TODO: Remove panic
-    let (ws_stream, _) = connect_async(socket_helper.uri)
-        .await
-        .expect("Websocket: Failed to connect");
-    // println!("WebSocket handshake has been successfully completed");
+    if let Ok((ws_stream, _)) = connect_async(&socket_helper.uri).await {
+        if let Some(on_open_msg) = socket_helper.on_open_msg {
+            // println!("Message sent: {}", on_open_msg);
+            stdin_tx
+                .unbounded_send(Message::binary(on_open_msg))
+                .unwrap();
+        } else {
+            // println!("Message NOT sent, because it's empty");
+        }
 
-    if let Some(on_open_msg) = socket_helper.on_open_msg {
-        // println!("Message sent: {}", on_open_msg);
-        stdin_tx
-            .unbounded_send(Message::binary(on_open_msg))
-            .unwrap();
+        let (write, read) = ws_stream.split();
+
+        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+        let ws_to_stdout = {
+            read.for_each(|message| async {
+                let message = message.unwrap().into_text().unwrap();
+                // println!("Got message: {:?}", mess);
+
+                socket_helper.callback.borrow()(socket_helper.pair.clone(), message);
+            })
+        };
+
+        pin_mut!(stdin_to_ws, ws_to_stdout);
+        future::select(stdin_to_ws, ws_to_stdout).await;
     } else {
-        // println!("Message NOT sent, because it's empty");
+        println!(
+            "Websocket: Failed to connect. Url: {}, pair: {}",
+            socket_helper.uri, socket_helper.pair
+        );
     }
-
-    let (write, read) = ws_stream.split();
-
-    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let message = message.unwrap().into_text().unwrap();
-            // println!("Got message: {:?}", mess);
-
-            socket_helper.callback.borrow()(socket_helper.pair.clone(), message);
-        })
-    };
-
-    pin_mut!(stdin_to_ws, ws_to_stdout);
-    future::select(stdin_to_ws, ws_to_stdout).await;
 }
