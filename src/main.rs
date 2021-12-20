cd4730dfea94345382c804d39897aff4b157aee0
Alexander Aglerimov <formiat95@gmail.com>
2021-12-20 15:14:45 +0300

    done: move logger configuration to service_config scheme

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use env_logger::{Env, DEFAULT_FILTER_ENV, DEFAULT_WRITE_STYLE_ENV};
+use env_logger::Builder;
 use std::sync::{mpsc, Arc, Mutex};
 
 use crate::repository::pair_average_trade_price::PairAverageTradePrice;
@@ -27,10 +27,10 @@ fn get_config(key: &str) -> config::Config {
     if let Some(path) = get_config_file_path(key) {
         market_config.merge(config::File::with_name(&path)).unwrap();
     } else {
-        let env_key = "APP_".to_string() + &key.to_uppercase();
+        let env_key = "APP__".to_string() + &key.to_uppercase() + "_";
 
         market_config
-            .merge(config::Environment::with_prefix(&env_key).separator("_"))
+            .merge(config::Environment::with_prefix(&env_key).separator("__"))
             .unwrap();
     }
 
@@ -49,11 +49,15 @@ fn get_param_value_as_vec_of_string(config: &config::Config, key: &str) -> Optio
 }
 
 fn main() {
-    let env = Env::default()
-        .filter_or(DEFAULT_FILTER_ENV, "index_daemon=trace")
-        .write_style_or(DEFAULT_WRITE_STYLE_ENV, "always");
+    let service_config = get_config("service_config");
 
-    env_logger::init_from_env(env);
+    let log_level: String = service_config
+        .get_str("log_level")
+        .unwrap_or("trace".to_string());
+
+    let mut builder = Builder::from_default_env();
+    builder.filter(Some("index_daemon"), log_level.parse().unwrap());
+    builder.init();
 
     let market_config = get_config("market_config");
 
