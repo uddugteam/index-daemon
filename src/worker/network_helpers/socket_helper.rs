d82585c6ab280967ad935ef90d8138e6a359af30
Alexander Aglerimov <formiat95@gmail.com>
2021-12-22 15:04:20 +0300

    done: refactoring - socket_helper - improved connect error message

--- src/worker/network_helpers/socket_helper.rs
+++ src/worker/network_helpers/socket_helper.rs
@@ -35,32 +35,33 @@ async fn run<F>(socket_helper: SocketHelper<F>)
 where
     F: Fn(String, String),
 {
-    let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
+    match connect_async(&socket_helper.uri).await {
+        Ok((ws_stream, _)) => {
+            let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
 
-    if let Ok((ws_stream, _)) = connect_async(&socket_helper.uri).await {
-        if let Some(on_open_msg) = socket_helper.on_open_msg {
-            stdin_tx
-                .unbounded_send(Message::binary(on_open_msg))
-                .unwrap();
-        }
-
-        let (write, read) = ws_stream.split();
-
-        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-        let ws_to_stdout = read.for_each(|message| async {
-            if let Ok(message) = message {
-                if let Ok(message) = message.into_text() {
-                    (socket_helper.callback)(socket_helper.pair.clone(), message);
-                }
+            if let Some(on_open_msg) = socket_helper.on_open_msg {
+                stdin_tx
+                    .unbounded_send(Message::binary(on_open_msg))
+                    .unwrap();
             }
-        });
 
-        pin_mut!(stdin_to_ws, ws_to_stdout);
-        future::select(stdin_to_ws, ws_to_stdout).await;
-    } else {
-        error!(
-            "Websocket: Failed to connect. Url: {}, pair: {}",
-            socket_helper.uri, socket_helper.pair
-        );
+            let (write, read) = ws_stream.split();
+
+            let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+            let ws_to_stdout = read.for_each(|message| async {
+                if let Ok(message) = message {
+                    if let Ok(message) = message.into_text() {
+                        (socket_helper.callback)(socket_helper.pair.clone(), message);
+                    }
+                }
+            });
+
+            pin_mut!(stdin_to_ws, ws_to_stdout);
+            future::select(stdin_to_ws, ws_to_stdout).await;
+        }
+        Err(e) => error!(
+            "Websocket: Failed to connect. Url: {}, pair: {}, error: {:?}",
+            socket_helper.uri, socket_helper.pair, e
+        ),
     }
 }
