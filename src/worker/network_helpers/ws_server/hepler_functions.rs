6b14742f5764b2f75cc6bf8f9e1da97cb6113834
Alexander Aglerimov <formiat95@gmail.com>
2022-02-09 13:34:08 +0300

    done: refactoring - moved "fn add_jsonrpc_version_and_method()" to "ws_server::helper_functions"

--- src/worker/network_helpers/ws_server/hepler_functions.rs
+++ src/worker/network_helpers/ws_server/hepler_functions.rs
@@ -1,9 +1,24 @@
-use crate::worker::helper_functions::add_jsonrpc_version_and_method;
 use crate::worker::network_helpers::ws_server::ws_channel_response::WsChannelResponse;
 use async_tungstenite::tungstenite::protocol::Message;
 use futures::channel::mpsc::{TrySendError, UnboundedSender};
 
 type Tx = UnboundedSender<Message>;
+
+pub fn add_jsonrpc_version_and_method(response: &mut String, method: Option<String>) {
+    let mut value: serde_json::Value = serde_json::from_str(response).unwrap();
+    let object = value.as_object_mut().unwrap();
+    object.insert(
+        "jsonrpc".to_string(),
+        serde_json::Value::from("2.0".to_string()),
+    );
+
+    if let Some(method) = method {
+        let object = object.get_mut("result").unwrap().as_object_mut().unwrap();
+        object.insert("method".to_string(), serde_json::Value::from(method));
+    }
+
+    *response = value.to_string();
+}
 
 pub fn ws_send_response(
     broadcast_recipient: &Tx,
