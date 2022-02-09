9026a86feb601b052b73649aefc6fdf388503189
Alexander Aglerimov <formiat95@gmail.com>
2022-02-09 19:24:59 +0300

    done: minor changes - ConfigScheme - derive Default

--- src/config_scheme/config_scheme.rs
+++ src/config_scheme/config_scheme.rs
@@ -1,23 +1,17 @@
 use crate::config_scheme::market_config::MarketConfig;
 use crate::config_scheme::service_config::ServiceConfig;
 
+#[derive(Default)]
 pub struct ConfigScheme {
     pub market: MarketConfig,
     pub service: ServiceConfig,
 }
+
 impl ConfigScheme {
     pub fn new() -> Self {
         Self {
             market: MarketConfig::new(),
             service: ServiceConfig::new(),
-        }
-    }
-}
-impl Default for ConfigScheme {
-    fn default() -> Self {
-        Self {
-            market: MarketConfig::default(),
-            service: ServiceConfig::default(),
         }
     }
 }
