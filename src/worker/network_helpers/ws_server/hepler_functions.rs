1e8e3834606cfb908fdb45ebac9e4eb6efdcaa12
Alexander Aglerimov <formiat95@gmail.com>
2022-02-21 20:14:22 +0300

    done: bugfix - "fn thin_by_interval()" - reworked algorithm

--- src/worker/network_helpers/ws_server/hepler_functions.rs
+++ src/worker/network_helpers/ws_server/hepler_functions.rs
@@ -2,7 +2,7 @@ use crate::worker::network_helpers::ws_server::interval::Interval;
 use crate::worker::network_helpers::ws_server::ws_channel_name::WsChannelName;
 use crate::worker::network_helpers::ws_server::ws_channel_response::WsChannelResponse;
 use async_tungstenite::tungstenite::protocol::Message;
-use chrono::{DateTime, Utc, MAX_DATETIME};
+use chrono::{DateTime, Utc, MIN_DATETIME};
 use futures::channel::mpsc::{TrySendError, UnboundedSender};
 
 type Tx = UnboundedSender<Message>;
@@ -44,23 +44,17 @@ pub fn thin_by_interval(
 ) -> Vec<(DateTime<Utc>, f64)> {
     values.sort_by(|a, b| a.0.cmp(&b.0));
 
-    let interval = interval.into_seconds();
+    let interval = interval.into_seconds() as i64;
 
     let mut res = Vec::new();
 
-    if let Some(mut last_el) = values.get(0).cloned() {
-        // Push dummy
-        values.push((MAX_DATETIME, 0.0));
+    let mut next_timestamp = MIN_DATETIME.timestamp();
+    for value in values {
+        let curr_timestamp = value.0.timestamp();
 
-        let mut last_timestamp = values[0].0.timestamp() as u64;
-
-        for el in values {
-            if el.0.timestamp() as u64 > last_timestamp {
-                res.push(last_el);
-                last_timestamp += interval;
-            }
-
-            last_el = el;
+        if curr_timestamp >= next_timestamp {
+            res.push(value);
+            next_timestamp = curr_timestamp + interval;
         }
     }
 
