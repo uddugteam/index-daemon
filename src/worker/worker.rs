cf6bd0eb643edb0906ed99e790dfe7251a134b64
Alexander Aglerimov <formiat95@gmail.com>
2022-03-24 16:23:45 +0300

    done: refactoring - removed "struct Worker"

--- src/worker/worker.rs
+++ src/worker/worker.rs
@@ -23,211 +23,207 @@ use std::sync::{Arc, Mutex, RwLock};
 use std::thread::{self, JoinHandle};
 use std::time;
 
-pub struct Worker {
-    tx: Sender<JoinHandle<()>>,
-    graceful_shutdown: Arc<RwLock<bool>>,
-    markets: HashMap<String, Arc<Mutex<dyn Market + Send>>>,
+fn is_graceful_shutdown(graceful_shutdown: &Arc<RwLock<bool>>) -> bool {
+    *graceful_shutdown.read().unwrap()
 }
 
-impl Worker {
-    pub fn new(tx: Sender<JoinHandle<()>>, graceful_shutdown: Arc<RwLock<bool>>) -> Self {
-        Self {
-            tx,
-            graceful_shutdown,
-            markets: HashMap::new(),
-        }
+fn configure(
+    market_names: Vec<&str>,
+    exchange_pairs: Vec<(String, String)>,
+    channels: Vec<MarketChannels>,
+    rest_timeout_sec: u64,
+    repositories: Option<MarketRepositoriesByMarketName>,
+    pair_average_price: StoredAndWsTransmissibleF64ByPairTuple,
+    index_price: Arc<RwLock<StoredAndWsTransmissibleF64>>,
+    index_pairs: Vec<(String, String)>,
+    percent_change_holder: &HolderHashMap<PercentChangeByInterval>,
+    percent_change_interval_sec: u64,
+    ws_channels_holder: &HolderHashMap<WsChannels>,
+    tx: Sender<JoinHandle<()>>,
+    graceful_shutdown: Arc<RwLock<bool>>,
+) -> HashMap<String, Arc<Mutex<dyn Market + Send>>> {
+    let mut markets = HashMap::new();
+    let mut repositories = repositories.unwrap_or_default();
+
+    for market_name in market_names {
+        let pair_average_price_2 = pair_average_price.clone();
+        let market_spine = MarketSpine::new(
+            pair_average_price_2,
+            Arc::clone(&index_price),
+            index_pairs.clone(),
+            tx.clone(),
+            rest_timeout_sec,
+            market_name.to_string(),
+            channels.clone(),
+            Arc::clone(&graceful_shutdown),
+        );
+        let market = market_factory(
+            market_spine,
+            exchange_pairs.clone(),
+            repositories.remove(market_name),
+            percent_change_holder,
+            percent_change_interval_sec,
+            ws_channels_holder,
+        );
+
+        markets.insert(market_name.to_string(), market);
     }
 
-    fn is_graceful_shutdown(&self) -> bool {
-        *self.graceful_shutdown.read().unwrap()
-    }
+    markets
+}
 
-    fn configure(
-        &mut self,
-        markets: Vec<&str>,
-        exchange_pairs: Vec<(String, String)>,
-        channels: Vec<MarketChannels>,
-        rest_timeout_sec: u64,
-        repositories: Option<MarketRepositoriesByMarketName>,
-        pair_average_price: StoredAndWsTransmissibleF64ByPairTuple,
-        index_price: Arc<RwLock<StoredAndWsTransmissibleF64>>,
-        index_pairs: Vec<(String, String)>,
-        percent_change_holder: &HolderHashMap<PercentChangeByInterval>,
-        percent_change_interval_sec: u64,
-        ws_channels_holder: &HolderHashMap<WsChannels>,
-    ) {
-        let mut repositories = repositories.unwrap_or_default();
+fn start_ws(
+    ws: bool,
+    ws_addr: String,
+    ws_answer_timeout_ms: u64,
+    index_price_repository: Option<RepositoryForF64ByTimestamp>,
+    pair_average_price: StoredAndWsTransmissibleF64ByPairTuple,
+    percent_change_holder: HolderHashMap<PercentChangeByInterval>,
+    ws_channels_holder: HolderHashMap<WsChannels>,
+    tx: Sender<JoinHandle<()>>,
+    graceful_shutdown: Arc<RwLock<bool>>,
+) {
+    if ws {
+        let percent_change_holder = PercentChangeByIntervalHolder::new(percent_change_holder);
+        let ws_channels_holder = WsChannelsHolder::new(ws_channels_holder);
 
-        for market_name in markets {
-            let pair_average_price_2 = pair_average_price.clone();
-            let market_spine = MarketSpine::new(
-                pair_average_price_2,
-                Arc::clone(&index_price),
-                index_pairs.clone(),
-                self.tx.clone(),
-                rest_timeout_sec,
-                market_name.to_string(),
-                channels.clone(),
-                Arc::clone(&self.graceful_shutdown),
-            );
-            let market = market_factory(
-                market_spine,
-                exchange_pairs.clone(),
-                repositories.remove(market_name),
-                percent_change_holder,
-                percent_change_interval_sec,
-                ws_channels_holder,
-            );
-
-            self.markets.insert(market_name.to_string(), market);
-        }
-    }
-
-    fn start_ws(
-        &self,
-        ws: bool,
-        ws_addr: String,
-        ws_answer_timeout_ms: u64,
-        index_price_repository: Option<RepositoryForF64ByTimestamp>,
-        pair_average_price: StoredAndWsTransmissibleF64ByPairTuple,
-        percent_change_holder: HolderHashMap<PercentChangeByInterval>,
-        ws_channels_holder: HolderHashMap<WsChannels>,
-        graceful_shutdown: Arc<RwLock<bool>>,
-    ) {
-        if ws {
-            let percent_change_holder = PercentChangeByIntervalHolder::new(percent_change_holder);
-            let ws_channels_holder = WsChannelsHolder::new(ws_channels_holder);
-
-            let thread_name = "fn: start_ws".to_string();
-            let thread = thread::Builder::new()
-                .name(thread_name)
-                .spawn(move || {
-                    let ws_server = WsServer {
-                        percent_change_holder,
-                        ws_channels_holder,
-                        ws_addr,
-                        ws_answer_timeout_ms,
-                        index_price_repository,
-                        pair_average_price,
-                        graceful_shutdown,
-                    };
-                    ws_server.start();
-                })
-                .unwrap();
-            self.tx.send(thread).unwrap();
-        }
-    }
-
-    fn start_db_cleaner(
-        &self,
-        index_price_repository: Option<RepositoryForF64ByTimestamp>,
-        pair_average_price_repositories: Option<WorkerRepositoriesByPairTuple>,
-        market_repositories: Option<MarketRepositoriesByMarketName>,
-        data_expire_sec: u64,
-    ) {
-        let thread_name = "fn: clear_db".to_string();
+        let thread_name = "fn: start_ws".to_string();
         let thread = thread::Builder::new()
             .name(thread_name)
             .spawn(move || {
-                loop {
-                    clear_db(
-                        index_price_repository.clone(),
-                        pair_average_price_repositories.clone(),
-                        market_repositories.clone(),
-                        data_expire_sec,
-                    );
-
-                    // Sleep 1 day
-                    thread::sleep(time::Duration::from_secs(86_400));
-                }
+                let ws_server = WsServer {
+                    percent_change_holder,
+                    ws_channels_holder,
+                    ws_addr,
+                    ws_answer_timeout_ms,
+                    index_price_repository,
+                    pair_average_price,
+                    graceful_shutdown,
+                };
+                ws_server.start();
             })
             .unwrap();
-        self.tx.send(thread).unwrap();
+        tx.send(thread).unwrap();
     }
+}
 
-    pub fn start(&mut self, config: ConfigScheme) {
-        let RepositoriesPrepared {
-            index_price_repository,
-            pair_average_price_repositories,
-            market_repositories,
-            percent_change_holder,
-            ws_channels_holder,
-            pair_average_price,
-            index_price,
-        } = RepositoriesPrepared::make(&config);
+fn start_db_cleaner(
+    index_price_repository: Option<RepositoryForF64ByTimestamp>,
+    pair_average_price_repositories: Option<WorkerRepositoriesByPairTuple>,
+    market_repositories: Option<MarketRepositoriesByMarketName>,
+    data_expire_sec: u64,
+    tx: Sender<JoinHandle<()>>,
+) {
+    let thread_name = "fn: clear_db".to_string();
+    let thread = thread::Builder::new()
+        .name(thread_name)
+        .spawn(move || {
+            loop {
+                clear_db(
+                    index_price_repository.clone(),
+                    pair_average_price_repositories.clone(),
+                    market_repositories.clone(),
+                    data_expire_sec,
+                );
 
-        let ConfigScheme {
-            market,
-            service,
-            matches: _,
-        } = config;
-        let MarketConfig {
-            markets,
-            exchange_pairs,
-            index_pairs,
-            channels,
-        } = market;
-        let ServiceConfig {
-            rest_timeout_sec,
-            ws,
-            ws_addr,
-            ws_answer_timeout_ms,
-            storage: _,
-            historical_storage_frequency_ms: _,
-            data_expire_sec,
-            percent_change_interval_sec,
-        } = service;
-
-        let markets = markets.iter().map(|v| v.as_ref()).collect();
-
-        self.configure(
-            markets,
-            exchange_pairs,
-            channels,
-            rest_timeout_sec,
-            market_repositories.clone(),
-            pair_average_price.clone(),
-            index_price,
-            index_pairs,
-            &percent_change_holder,
-            percent_change_interval_sec,
-            &ws_channels_holder,
-        );
-        self.start_ws(
-            ws,
-            ws_addr,
-            ws_answer_timeout_ms,
-            index_price_repository.clone(),
-            pair_average_price,
-            percent_change_holder,
-            ws_channels_holder,
-            self.graceful_shutdown.clone(),
-        );
-
-        self.start_db_cleaner(
-            index_price_repository,
-            pair_average_price_repositories,
-            market_repositories,
-            data_expire_sec,
-        );
-
-        for market in self.markets.values().cloned() {
-            if self.is_graceful_shutdown() {
-                return;
+                // Sleep 1 day
+                thread::sleep(time::Duration::from_secs(86_400));
             }
+        })
+        .unwrap();
+    tx.send(thread).unwrap();
+}
 
-            let thread_name = format!(
-                "fn: market_update, market: {}",
-                market.lock().unwrap().get_spine().name
-            );
-            let thread = thread::Builder::new()
-                .name(thread_name)
-                .spawn(move || {
-                    market_update(market);
-                })
-                .unwrap();
-            self.tx.send(thread).unwrap();
+pub fn start_worker(
+    config: ConfigScheme,
+    tx: Sender<JoinHandle<()>>,
+    graceful_shutdown: Arc<RwLock<bool>>,
+) {
+    let RepositoriesPrepared {
+        index_price_repository,
+        pair_average_price_repositories,
+        market_repositories,
+        percent_change_holder,
+        ws_channels_holder,
+        pair_average_price,
+        index_price,
+    } = RepositoriesPrepared::make(&config);
+
+    let ConfigScheme {
+        market,
+        service,
+        matches: _,
+    } = config;
+    let MarketConfig {
+        markets,
+        exchange_pairs,
+        index_pairs,
+        channels,
+    } = market;
+    let ServiceConfig {
+        rest_timeout_sec,
+        ws,
+        ws_addr,
+        ws_answer_timeout_ms,
+        storage: _,
+        historical_storage_frequency_ms: _,
+        data_expire_sec,
+        percent_change_interval_sec,
+    } = service;
+
+    let markets = markets.iter().map(|v| v.as_ref()).collect();
+
+    let markets = configure(
+        markets,
+        exchange_pairs,
+        channels,
+        rest_timeout_sec,
+        market_repositories.clone(),
+        pair_average_price.clone(),
+        index_price,
+        index_pairs,
+        &percent_change_holder,
+        percent_change_interval_sec,
+        &ws_channels_holder,
+        tx.clone(),
+        graceful_shutdown.clone(),
+    );
+    start_ws(
+        ws,
+        ws_addr,
+        ws_answer_timeout_ms,
+        index_price_repository.clone(),
+        pair_average_price,
+        percent_change_holder,
+        ws_channels_holder,
+        tx.clone(),
+        graceful_shutdown.clone(),
+    );
+
+    start_db_cleaner(
+        index_price_repository,
+        pair_average_price_repositories,
+        market_repositories,
+        data_expire_sec,
+        tx.clone(),
+    );
+
+    for (_market_name, market) in markets {
+        if is_graceful_shutdown(&graceful_shutdown) {
+            return;
         }
+
+        let thread_name = format!(
+            "fn: market_update, market: {}",
+            market.lock().unwrap().get_spine().name
+        );
+        let thread = thread::Builder::new()
+            .name(thread_name)
+            .spawn(move || {
+                market_update(market);
+            })
+            .unwrap();
+        tx.send(thread).unwrap();
     }
 }
 
@@ -352,7 +348,7 @@ pub mod test {
             thread_names.push(thread_name);
         }
 
-        worker.start(config);
+        worker.start_worker(config);
         check_threads(thread_names, rx);
     }
 
