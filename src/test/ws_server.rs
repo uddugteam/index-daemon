8c33220c4d2e5eb82ece7e54fe9273cbba4e6c51
Alexander Aglerimov <formiat95@gmail.com>
2022-03-29 12:10:25 +0300

    done: tests - WsServer - removed old test "test_ws_channels_response"

--- src/test/ws_server.rs
+++ src/test/ws_server.rs
@@ -11,13 +11,11 @@ use crate::worker::network_helpers::ws_server::ws_channel_name::WsChannelName;
 use crate::worker::worker::start_worker;
 use serde_json::json;
 use serial_test::serial;
-use std::collections::HashMap;
 use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{mpsc, Arc, RwLock};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time;
-use std::time::Instant;
 use uuid::Uuid;
 
 fn start_application(
@@ -94,71 +92,6 @@ fn ws_connect_and_subscribe(
 
     // Give Websocket server time to process request
     thread::sleep(time::Duration::from_millis(1000));
-}
-
-fn check_incoming_messages(
-    incoming_msg_rx: Receiver<String>,
-    expected: Vec<(JsonRpcId, WsChannelName, Vec<String>, Option<Vec<String>>)>,
-) {
-    let mut methods = HashMap::new();
-    for (sub_id, method, ..) in &expected {
-        methods.insert(sub_id.clone(), *method);
-    }
-
-    let mut expected_new = HashMap::new();
-    for (sub_id, method, coins, exchanges) in expected {
-        for coin in coins {
-            if let Some(exchanges) = exchanges.clone() {
-                for exchange in exchanges {
-                    expected_new.insert((sub_id.clone(), method, coin.clone(), Some(exchange)), ());
-                }
-            } else {
-                expected_new.insert((sub_id.clone(), method, coin, None), ());
-            }
-        }
-    }
-
-    let start = Instant::now();
-    while !expected_new.is_empty() {
-        if let Ok(incoming_msg) = incoming_msg_rx.try_recv() {
-            let incoming_msg: serde_json::Value = serde_json::from_str(&incoming_msg).unwrap();
-
-            let sub_id = incoming_msg.get("id").unwrap().as_str().unwrap();
-            let sub_id = JsonRpcId::Str(sub_id.to_string());
-
-            let jsonrpc = incoming_msg.get("jsonrpc").unwrap().as_str().unwrap();
-            assert_eq!(jsonrpc, "2.0");
-
-            let result = incoming_msg.get("result").unwrap().as_object().unwrap();
-            if let Some(message) = result.get("message") {
-                // SuccSub message (`result` has `message` field)
-
-                let message = message.as_str().unwrap();
-                assert_eq!(message, "Successfully subscribed.");
-            } else {
-                // Message with payload (no `message` field in `result`)
-
-                let coin = result.get("coin").unwrap().as_str().unwrap().to_string();
-                let exchange = result
-                    .get("exchange")
-                    .map(|v| v.as_str().unwrap().to_string());
-                let _value = result.get("value").unwrap().as_f64().unwrap();
-                let timestamp = result.get("timestamp").unwrap().as_i64().unwrap();
-                // 1640984400 = 2022-01-01 00:00:00
-                assert!(timestamp > 1640984400);
-                let method = *methods.get(&sub_id).unwrap();
-
-                expected_new.remove(&(sub_id, method, coin, exchange));
-            }
-        }
-
-        let minutes = 4;
-        if start.elapsed().as_secs() > minutes * 60 {
-            panic!("Allocated time ({} minutes) is over.", minutes);
-        }
-
-        thread::sleep(time::Duration::from_millis(100));
-    }
 }
 
 fn zip_coins_with_usd(coins: Option<Vec<String>>) -> Vec<Option<(String, String)>> {
@@ -319,42 +252,4 @@ fn test_worker_add_ws_channels() {
 
     ws_connect_and_subscribe(ws_addr, requests, incoming_msg_tx);
     check_subscriptions(repositories_prepared, expected);
-}
-
-#[test]
-#[ignore]
-#[serial]
-/// TODO: Fix (not always working on github)
-fn test_ws_channels_response() {
-    let ws_addr = "127.0.0.1:8000";
-    let (_rx, (incoming_msg_tx, incoming_msg_rx), _) = start_application(ws_addr);
-
-    let mut requests = Vec::new();
-    let mut expected = Vec::new();
-
-    let sub_id = JsonRpcId::Str(Uuid::new_v4().to_string());
-    let method = WsChannelName::CoinAveragePrice;
-    let coins = ["BTC".to_string()].to_vec();
-    let request = make_request(&sub_id, method, Some(&coins), None);
-    requests.push(request);
-    expected.push((sub_id, method, coins, None));
-
-    let sub_id = JsonRpcId::Str(Uuid::new_v4().to_string());
-    let method = WsChannelName::CoinExchangePrice;
-    let coins = ["BTC".to_string()].to_vec();
-    let exchanges = ["binance".to_string()].to_vec();
-    let request = make_request(&sub_id, method, Some(&coins), Some(&exchanges));
-    requests.push(request);
-    expected.push((sub_id, method, coins, Some(exchanges)));
-
-    let sub_id = JsonRpcId::Str(Uuid::new_v4().to_string());
-    let method = WsChannelName::CoinExchangeVolume;
-    let coins = ["BTC".to_string(), "ETH".to_string()].to_vec();
-    let exchanges = ["binance".to_string(), "coinbase".to_string()].to_vec();
-    let request = make_request(&sub_id, method, Some(&coins), Some(&exchanges));
-    requests.push(request);
-    expected.push((sub_id, method, coins, Some(exchanges)));
-
-    ws_connect_and_subscribe(ws_addr, requests, incoming_msg_tx);
-    check_incoming_messages(incoming_msg_rx, expected);
 }
