de8f7688016b2cce81e7de95349c67ed3d9bdf1e
Alexander Aglerimov <formiat95@gmail.com>
2022-03-29 19:17:21 +0300

    done: WsRequest - refactoring - added "invalid params" error messages

--- src/worker/network_helpers/ws_server/ws_request.rs
+++ src/worker/network_helpers/ws_server/ws_request.rs
@@ -17,12 +17,15 @@ pub enum WsRequest {
 
 impl WsRequest {
     pub fn new(request: JsonRpcRequest, ws_answer_timeout_ms: u64) -> Result<Self, String> {
-        let e = "Wrong params.";
         let id = request.id.clone();
-        let object = request.params.as_object().ok_or(e)?;
-        let coins = Self::parse_vec_of_str(object, "coins").ok_or(e);
+        let object = request
+            .params
+            .as_object()
+            .ok_or("\"params\" is required and must be an object")?;
+        let coins = Self::parse_vec_of_str(object, "coins");
 
-        let frequency_ms = Self::parse_u64(object, "frequency_ms").unwrap_or(ws_answer_timeout_ms);
+        let frequency_ms =
+            Self::parse_u64(object, "frequency_ms").unwrap_or(Ok(ws_answer_timeout_ms))?;
         if frequency_ms < ws_answer_timeout_ms {
             return Err(format!(
                 "\"frequency_ms\" must be not less than {}",
@@ -30,15 +33,27 @@ impl WsRequest {
             ));
         }
 
-        let percent_change_interval_sec = object
-            .get("percent_change_interval")
-            .map(|v| v.as_str().map(|v| parse(v).unwrap().as_secs()).ok_or(e));
+        let percent_change_interval_sec = object.get("percent_change_interval").map(|v| {
+            v.as_str()
+                .map(|v| {
+                    parse(v)
+                        .map(|v| v.as_secs())
+                        .map_err(|_| "\"percent_change_interval\": invalid value")
+                })
+                .ok_or("\"percent_change_interval\" must be a string")?
+        });
         let percent_change_interval_sec = Self::swap(percent_change_interval_sec);
         let interval_sec = object
             .get("interval")
-            .ok_or(e)
-            .map(|v| v.as_str().ok_or(e))
-            .map(|v| v.map(|v| parse(v).map(|v| v.as_secs()).map_err(|_| e)));
+            .ok_or("\"interval\" is required")
+            .map(|v| v.as_str().ok_or("\"interval\" must be a string"))
+            .map(|v| {
+                v.map(|v| {
+                    parse(v)
+                        .map(|v| v.as_secs())
+                        .map_err(|_| "\"interval\": invalid value")
+                })
+            });
 
         match request.method {
             WsChannelName::AvailableCoins => {
@@ -92,7 +107,7 @@ impl WsRequest {
             }
             WsChannelName::CoinExchangePrice | WsChannelName::CoinExchangeVolume => {
                 let coins = coins?;
-                let exchanges = Self::parse_vec_of_str(object, "exchanges").ok_or(e)?;
+                let exchanges = Self::parse_vec_of_str(object, "exchanges")?;
 
                 let res = match request.method {
                     WsChannelName::CoinExchangePrice => MarketChannels::CoinExchangePrice {
@@ -123,10 +138,11 @@ impl WsRequest {
             | WsChannelName::IndexPriceCandlesHistorical
             | WsChannelName::CoinAveragePriceHistorical
             | WsChannelName::CoinAveragePriceCandlesHistorical => {
-                let coin = object.get("coin").ok_or(e);
+                let coin = object.get("coin").ok_or("\"coin\" is required");
                 let interval_sec = interval_sec???;
-                let from = Self::parse_u64(object, "from").ok_or(e)?;
-                let to = Self::parse_u64(object, "to").ok_or(e).ok();
+                let from = Self::parse_u64(object, "from").ok_or("\"from\" is required")??;
+                let to = Self::parse_u64(object, "to");
+                let to = Self::swap(to)?;
 
                 let res = match request.method {
                     WsChannelName::IndexPriceHistorical => WsMethodRequest::IndexPriceHistorical {
@@ -146,7 +162,7 @@ impl WsRequest {
                     WsChannelName::CoinAveragePriceHistorical => {
                         WsMethodRequest::CoinAveragePriceHistorical {
                             id,
-                            coin: coin?.as_str().ok_or(e)?.to_string(),
+                            coin: coin?.as_str().ok_or("\"coin\" must be string")?.to_string(),
                             interval_sec,
                             from,
                             to,
@@ -155,7 +171,7 @@ impl WsRequest {
                     WsChannelName::CoinAveragePriceCandlesHistorical => {
                         WsMethodRequest::CoinAveragePriceCandlesHistorical {
                             id,
-                            coin: coin?.as_str().ok_or(e)?.to_string(),
+                            coin: coin?.as_str().ok_or("\"coin\" must be string")?.to_string(),
                             interval_sec,
                             from,
                             to,
@@ -169,17 +185,34 @@ impl WsRequest {
         }
     }
 
-    fn parse_vec_of_str(object: &Map<String, serde_json::Value>, key: &str) -> Option<Vec<String>> {
+    fn parse_vec_of_str(
+        object: &Map<String, serde_json::Value>,
+        key: &str,
+    ) -> Result<Vec<String>, String> {
         let mut items = Vec::new();
-        for item in object.get(key)?.as_array()? {
-            items.push(item.as_str()?.to_string());
+        for item in object
+            .get(key)
+            .ok_or(format!("\"{}\" is required", key))?
+            .as_array()
+            .ok_or(format!("\"{}\" must be an array", key))?
+        {
+            items.push(
+                item.as_str()
+                    .ok_or(format!("\"{}\" must be an array of string", key))?
+                    .to_string(),
+            );
         }
 
-        Some(items)
+        Ok(items)
     }
 
-    fn parse_u64(object: &Map<String, serde_json::Value>, key: &str) -> Option<u64> {
-        object.get(key)?.as_u64()
+    fn parse_u64(
+        object: &Map<String, serde_json::Value>,
+        key: &str,
+    ) -> Option<Result<u64, String>> {
+        object
+            .get(key)
+            .map(|v| v.as_u64().ok_or(format!("\"{}\" must be an uint", key)))
     }
 
     fn swap<T, E>(value: Option<Result<T, E>>) -> Result<Option<T>, E> {
