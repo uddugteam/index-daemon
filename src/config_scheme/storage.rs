e5df6168b0cf000bc28b166375673538eb9afa34
Alexander Aglerimov <formiat95@gmail.com>
2022-07-25 17:10:18 +0400

    done: added RAM debug print before and after DB open

--- src/config_scheme/storage.rs
+++ src/config_scheme/storage.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 use std::sync::Arc;
+use sysinfo::{System, SystemExt};
 use tokio::sync::RwLock;
+
+#[derive(Debug)]
+enum RamCheckTime {
+    Before,
+    After,
+}
 
 #[derive(Clone)]
 pub enum Storage {
@@ -9,23 +16,40 @@ pub enum Storage {
 }
 
 impl Storage {
+    pub fn new(name: &str) -> Result<Self, String> {
+        let mut sys = System::new_all();
+        Self::check_ram(&mut sys, RamCheckTime::Before);
+
+        let res = match name {
+            "cache" => Ok(Self::Cache(Arc::new(RwLock::new(HashMap::new())))),
+            "sled" => Ok(Self::make_sled()),
+            other_storage => Err(format!("Got wrong storage name: {}", other_storage)),
+        };
+
+        Self::check_ram(&mut sys, RamCheckTime::After);
+
+        res
+    }
+
     fn make_sled() -> Self {
         let tree = vsdbsled::open("db").expect("Open db error.");
 
         Self::Sled(tree)
     }
 
-    pub fn from_str(name: &str) -> Result<Self, String> {
-        match name {
-            "cache" => Ok(Self::Cache(Arc::new(RwLock::new(HashMap::new())))),
-            "sled" => Ok(Self::make_sled()),
-            other_storage => Err(format!("Got wrong storage name: {}", other_storage)),
-        }
+    fn check_ram(sys: &mut System, time: RamCheckTime) {
+        sys.refresh_all();
+        let used_memory = sys.used_memory() / 1024;
+        let total_memory = sys.total_memory() / 1024;
+        debug!(
+            "RAM {:?} DB open: {}/{} MB",
+            time, used_memory, total_memory,
+        );
     }
 }
 
 impl Default for Storage {
     fn default() -> Self {
-        Self::make_sled()
+        Self::new("sled").unwrap()
     }
 }
